3b9f32c6686cce33d2474c9dbed03f1d60233f0b
hyunzsu <hyunzsu@naver.com>
2024-11-11 16:53:47 +0900

    chore: ReactQueryDevtools 적용

--- src/app/config/ReactQueryClientProvider.tsx
+++ src/app/config/ReactQueryClientProvider.tsx
@@ -1,11 +1,17 @@
 'use client';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 // React Query는 내부적으로 queryClient를 사용하여
 // 각종 상태를 저장하고, 부가 기능을 제공함
 export const queryClient = new QueryClient({});
 
 export default function ReactQueryClientProvider({ children }: React.PropsWithChildren) {
-  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 }
