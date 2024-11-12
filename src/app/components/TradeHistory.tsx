de62ae7f490cbe42bb8f925e040636913b72322d
hyunzsu <hyunzsu@naver.com>
2024-11-12 10:00:12 +0900

    feat: TradeHistory 컴포넌트 임시 목업데이터 제거 및 실제 통신 구현

--- src/app/components/TradeHistory.tsx
+++ src/app/components/TradeHistory.tsx
@@ -1,11 +1,17 @@
-import { tradeHistoryData } from '../mocks';
+'use client';
+
+import { useTradeHistory } from '@/app/hooks/queries/useTradeHistory';
 import { formatNumber } from '../utils/format';
 
 export default function TradeHistory() {
+  const { data } = useTradeHistory('BTCUSDT');
+
+  if (!data?.trades) return null;
+
   return (
-    <section className="common-bg common-border-radius flex h-full flex-col">
+    <section className="common-bg common-border-radius flex h-full max-h-[344px] w-[364px] flex-col">
       {/* Header */}
-      <div className="border-colorLine flex items-center justify-between border-b px-4 py-2">
+      <div className="flex items-center justify-between border-b border-colorLine px-4 py-2">
         <div className="text-textPrimary text-sm font-bold">Market Trades</div>
       </div>
 
@@ -18,7 +24,7 @@ export default function TradeHistory() {
 
       {/* Trade List */}
       <div className="flex-1 overflow-auto">
-        {tradeHistoryData.map((trade) => {
+        {data.trades.map((trade) => {
           const time = new Date(trade.time).toLocaleTimeString('en-US', {
             hour12: false,
             hour: '2-digit',
@@ -28,11 +34,11 @@ export default function TradeHistory() {
 
           return (
             <div key={trade.id} className="hover:bg-background-light grid grid-cols-3 px-4 py-1 text-xs">
-              <div className={`${trade.isBuyerMaker ? 'text-pink' : 'text-green'}`}>
+              <div className={`font-medium ${trade.isBuyerMaker ? 'text-pink' : 'text-green'}`}>
                 {formatNumber(parseFloat(trade.price), 2)}
               </div>
-              <div className="text-textPrimary text-right">{trade.qty}</div>
-              <div className="text-right text-textTertiary">{time}</div>
+              <div className="text-textPrimary text-right font-medium">{trade.qty}</div>
+              <div className="text-right font-medium text-textTertiary">{time}</div>
             </div>
           );
         })}
